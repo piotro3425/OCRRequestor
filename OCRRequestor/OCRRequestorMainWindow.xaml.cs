7abb881f8ecf960332847c60e31987ff666cc23a
PiotrO <piotro3425@gmail.com>
2021-09-23 19:40:55 +0200

    Create initial empty WPF project.

---
+++ OCRRequestor/OCRRequestorMainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace OCRRequestor
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class OCRRequestorMainWindow : Window
+   {
+      public OCRRequestorMainWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
